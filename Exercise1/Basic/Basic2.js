764a346a3aa2cddd4e8085c51e58cff31e1b0df2
JannnikJia <81467540+Jannik-Jia@users.noreply.github.com>
2022-11-01 07:21:47 +0800

    Update Basic2.js

--- Exercise1/Basic/Basic2.js
+++ Exercise1/Basic/Basic2.js
@@ -1,6 +1,7 @@
 "use strict"
 
 function drawArcCircle(canvas) {
+
     let context = canvas.getContext("2d");
 
     //TODO 1.2)       Use the arc() function to
@@ -8,5 +9,19 @@ function drawArcCircle(canvas) {
     //                from Task 1.1.
 
 
+    // ring 
+    context.beginPath();
+    context.fillStyle = 'red';
+    context.arc(100,100,50,0,2*Math.PI);
+    context.fill();
 
+    // filled
+    var width = 10;
+    context.lineWidth = width;
+    context.strokeStyle = 'green';
+    context.arc(100,100,50,0,2*Math.PI);
+    context.stroke();
+
+
+    // but i'm not sure how to apply colors
 }
