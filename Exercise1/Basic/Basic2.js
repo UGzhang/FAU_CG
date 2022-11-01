ce8504db45b73b1704c8bd52269afaabf93e9cf3
JannnikJia <81467540+Jannik-Jia@users.noreply.github.com>
2022-11-01 08:49:45 +0800

    Update Basic2.js

--- Exercise1/Basic/Basic2.js
+++ Exercise1/Basic/Basic2.js
@@ -8,20 +8,21 @@ function drawArcCircle(canvas) {
     //                rasterize the two circles
     //                from Task 1.1.
 
-
-    // ring 
+    // circle without contour
     context.beginPath();
-    context.fillStyle = 'red';
-    context.arc(100,100,50,0,2*Math.PI);
+    context.fillStyle = 'green';
+    context.arc(60,60,50,0,2*Math.PI);
     context.fill();
+    // context.stroke();
 
-    // filled
+    
+    // circle with contour
+    context.beginPath();
     var width = 10;
     context.lineWidth = width;
     context.strokeStyle = 'green';
-    context.arc(100,100,50,0,2*Math.PI);
+    context.arc(140,140,50,0,2*Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
     context.stroke();
-
-
-    // but i'm not sure how to apply colors
 }
